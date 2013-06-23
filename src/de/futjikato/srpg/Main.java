13194b732356d9b3978eaa5af2c4f3dc5c0d3662
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2013-06-23 18:50:41 +0200

    added framecounter and adjusted native links

--- src/de/futjikato/srpg/Main.java
+++ src/de/futjikato/srpg/Main.java
@@ -3,6 +3,7 @@ package de.futjikato.srpg;
 import de.futjikato.segine.SegineException;
 import de.futjikato.segine.TextureManager;
 import de.futjikato.segine.map.Map;
+import de.futjikato.segine.rendering.FrameCounter;
 import de.futjikato.segine.rendering.Renderer;
 import org.newdawn.slick.Input;
 
@@ -22,11 +23,19 @@ public class Main {
             // initialize renderer
             Renderer renderer = new Renderer();
 
+            // add framecounter
+            renderer.setFrameCounter(new FrameCounter() {
+                @Override
+                public void update(int frames) {
+                    System.out.println(frames);
+                }
+            });
+
             TextureManager.getInstance().setColorKey(0, 0, 0, "game/ground/dev.png");
             TextureManager.getInstance().setColorKey(255, 255, 255, "game/ground/grass02.png");
 
             // create map object and add to renderer
-            Map map = new Map("levels/test01.png");
+            Map map = new Map("assets\\levels\\test01.png");
             renderer.addRenderContainer(map);
 
             renderer.start();
@@ -38,9 +47,9 @@ public class Main {
     public static void defineLwjglLibraryPath() throws Exception {
         String os = System.getProperty("os.name");
         if (os.toLowerCase().contains("windows")) {
-            System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "\\libs\\lwjgl-2.9.0\\native\\windows");
+            System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "\\..\\segine\\libs\\lwjgl-2.9.0\\native\\windows");
         } else if (os.toLowerCase().contains("mac")) {
-            System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "/libs/lwjgl-2.9.0/native/macosx");
+            System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "/../segine/libs/lwjgl-2.9.0/native/macosx");
         } else {
             throw new Exception("For this os we haven´t jet assigned the native libs : \"" + os + "\"");
         }
